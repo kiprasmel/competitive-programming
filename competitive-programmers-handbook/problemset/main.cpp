33696caa6531e7a3233d53f29053f6315e0c2dbc
Kipras Melnikovas <kipras@kipras.org>
2020-09-24 00:34:47 +0300

    main.cpp: ll, optimize, LOG, freopen

--- competitive-programmers-handbook/problemset/main.cpp
+++ competitive-programmers-handbook/problemset/main.cpp
@@ -1,21 +1,31 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
+typedef long long ll;
 typedef unsigned long long ull;
 
 #if (0 || defined(DEBUG)) && !defined(PROD)
 #define LOG(x) std::cout << x << "\n"
+#define LOGS(x) std::cout << x << " "
+#define LO(x) std::cout << x
 #else
 #define LOG(x)
+#define LOGS(x)
+#define LO(x)
 #endif
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	// freopen("in", "r", stdin); std::ifstream __input_file("in"); assert(__input_file.good());
+	// freopen("out", "w", stdout);
+
 	
 
 	return 0;
 }
-

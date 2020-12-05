6452c5e9f7c0bb85e52a0fa38a8a6693c09d19a1
Kipras Melnikovas <kipras@kipras.org>
2020-12-05 16:11:25 +0200

    day 5 aoc 2020
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ advent-of-code/2020/05.hs
@@ -0,0 +1,17 @@
+binarySearch :: String -> Int -> Int -> Int -> Char -> Char -> Int
+binarySearch row rowIdx from to lt gte
+  | from == to  = to
+  | char == lt  = binarySearch row (rowIdx + 1) (from   ) (mid) lt gte
+  | char == gte = binarySearch row (rowIdx + 1) (mid + 1) (to ) lt gte
+  where
+    char = row !! rowIdx
+    mid  = (from + to) `div` 2
+
+calcSeatId :: (String, String)-> Int
+calcSeatId rowCol = (binarySearch row 0 0 127 'F' 'B') * 8 + (binarySearch col 0 0 7 'L' 'R')
+  where
+    row = fst rowCol
+    col = snd rowCol
+
+main :: IO ()
+main = interact $ show . maximum . map (calcSeatId . splitAt 7) . lines
